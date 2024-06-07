c44da965fd78ec9ef347e381f5134d6eb597f7b3
Dmitry Naumov <komrad.nda@yandex.ru>
2024-06-08 00:49:29 +0300

    Fix-4

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,7 @@ export const HW2 = () => {
 	let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
 	const filterUsers = () => {
-		const filteredUsers = users.myFriends.filter(city => city.address.city === 'Los Angeles');
+		const filteredUsers = users.myFriends.filter(c => c.address.city === 'Los Angeles');
 		setCurrentUsers({myFriends: filteredUsers});
 	};
 
